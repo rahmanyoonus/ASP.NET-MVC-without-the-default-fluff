4cbba776e1fd2bacb38cf3bba1ec42a770c97425
lidd4 <14b4087@ubd.edu.bn>
2018-01-23 03:30:41 +0800

    It all begins here

---
+++ VillageRun/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace VillageRun
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js")
+                .Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js")
+                .Include("~/Scripts/bootstrap.js","~/Scripts/respond.js"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css")
+                .Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/Style.css"));
+
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+        }
+    }
+
+
+}
---
+++ VillageRun/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace VillageRun
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        }
+    }
+}
